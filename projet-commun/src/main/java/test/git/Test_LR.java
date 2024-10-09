31ed37ada88e020acab5504ea041a624a20c2815
Roe121 <lezzarrami@gmail.com>
2024-10-09 11:17:50 +0200

    Start + Stop

--- projet-commun/src/main/java/test/git/Test_LR.java
+++ projet-commun/src/main/java/test/git/Test_LR.java
@@ -7,13 +7,13 @@ public class Test_LR {
 
 	public static void main(String[] args) {
 		
-		System.out.println( "Début :) " );
+		System.out.println( "Start :) " );
 
 		System.out.printf( "%nBonjour. " );
 		System.out.printf("Nous sommes le %1$tA %1$te %1$tB %1$tY. ", LocalDate.now() );
 		System.out.printf("Il est %1$tHh%1$tM.%n%n", LocalTime.now() );
 		
-		System.out.println( "Fin :)"  );
+		System.out.println( "Stop :)"  );
 
 	}
 
