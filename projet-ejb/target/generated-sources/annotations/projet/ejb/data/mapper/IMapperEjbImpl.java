bb014446e212effe2394f510eec40a5da841c5ee
Roe121 <lezzarrami@gmail.com>
2024-10-09 11:04:52 +0200

    Création Test_LR

--- projet-ejb/target/generated-sources/annotations/projet/ejb/data/mapper/IMapperEjbImpl.java
+++ projet-ejb/target/generated-sources/annotations/projet/ejb/data/mapper/IMapperEjbImpl.java
@@ -7,7 +7,7 @@ import projet.ejb.data.Compte;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-10-08T15:45:24+0200",
+    date = "2024-10-09T10:52:54+0200",
     comments = "version: 1.5.5.Final, compiler: Eclipse JDT (IDE) 1.4.100.v20220318-0906, environment: Java 17.0.12 (Eclipse Adoptium)"
 )
 @ApplicationScoped
